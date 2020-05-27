f5587c7d0e5a718f8923fe20491d7ee2eb58a1fc
JohnAsare <johnasare7@gmail.com>
2020-05-27 19:42:30 -0400

    if applies, this commit will print a list comprehension of first letters in a sentences

---
+++ q6.py
@@ -0,0 +1,10 @@
+# John Asare
+# May 27 2020
+
+
+""" Use List Comprehension to create a list of the first letters of every word in the string below:"""
+
+st = 'Create a list of the first letters of every word in this string'
+
+first_letter = [word[0] for word in st.split()]
+print(first_letter)
