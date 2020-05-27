18905fd6b5214c6ebad6e8f050e7f35d72d963d1
JohnAsare <johnasare7@gmail.com>
2020-05-27 19:28:11 -0400

    Adding more code to check for even numbers

--- q2.py
+++ q2.py
@@ -4,6 +4,15 @@
 
 """ Use range() to print all the even numbers from 0 to 10."""
 
+print('Using computation')
 for number in range(0, 11):
     if number % 2 == 0:
         print(number)
+
+print('\nusing range steps of 2')
+for number in range(0, 11, 2):
+    print(number)
+
+print('\nUsing list and range')
+print(list(range(0, 11, 2)))
+
