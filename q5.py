4e9dc2ca4d51d68a264bb8d0729bbbfa3c84d817
JohnAsare <johnasare7@gmail.com>
2020-05-27 19:09:47 -0400

    Adding the code to check if number is multiples of 3 or 5. Then for 3's it will print FIzz but Buzz for 5's

---
+++ q5.py
@@ -0,0 +1,16 @@
+# John Asare
+# may 27 2020
+
+
+""" Write a program that prints the integers from 1 to 100. But for multiples of three print "Fizz"
+instead of the number, and for the multiples of five print "Buzz".
+For numbers which are multiples of both three and five print "FizzBuzz"."""
+
+
+for number in range(1, 101):
+    if number % 3 == 0:
+        print(f'{number}\tFizz')
+    elif number % 5 == 0:
+        print('Buzz')
+    else:
+        print(number)
